4f0bd518d935eeb98bbd4c561e97070f83bc2eb3
CharlesMartel <CharlesMartel>
2012-02-20 13:22:12 -0500

    Changing computers. Committing to access at home

---
+++ Presto.Common/Log.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Presto.Common
+{
+	/// <summary>
+	/// A common log for all program errors or debug statements to be written to
+	/// </summary>
+	public static class Log
+	{
+	
+	}
+}
--- Presto.Common/Net/TCPServer.cs
+++ Presto.Common/Net/TCPServer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using Presto.Common;
 
 namespace Presto.Common.Net
 {
@@ -22,7 +23,16 @@ namespace Presto.Common.Net
 		/// </param>
 		public TCPServer (int port)
 		{
-			
+			IPEndPoint ipEndpoint = new IPEndPoint (IPAddress.Any, port);
+			listener = new Socket (ipEndpoint.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+	
+			//try to bind the enpoint and start the listener
+			try {
+				listener.Bind (ipEndpoint);
+				listener.Listen (10); // the integer passed to listen specifies a maximum backlag size. Im not entirely sure what that actually means though. 
+			} catch (Exception e) {
+				//TODO: write exception to error log
+			}			
 		}
 	}
 }
