9606b0e6a70546148bc70b6a7ea5dfa8e161d06c
Charles Martel <charleythehammer@gmail.com>
2012-02-19 22:53:04 -0500

    Updates and changes to structure

--- Presto/AssemblyInfo.cs
+++ Presto/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("hitsynth")]
+[assembly: AssemblyCopyright("Charles Martel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
